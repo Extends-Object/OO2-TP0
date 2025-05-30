bd91f493268fbeeaaa72a5e0de9b66f016c3a43e
Extends-Object <return.unstring@gmail.com>
2025-05-30 19:02:00 -0300

    Corrección de la resolución - Separación en Anemicos y No anemicos

--- src/no_anemicos/Main_no_anemico.java
+++ src/no_anemicos/Main_no_anemico.java
@@ -13,13 +13,17 @@ public class Main_no_anemico {
 
         Tiempo a = new Tiempo();
 
-        //Los metodos usan el estado interno en vez crear una fecha nueva cada llamada a los metodos
-        //En el constructor instancia la fecha actual y ese dato establece el estado interno del objeto
+        /*
+        Los metodos usan el estado interno en vez crear una fecha nueva cada llamada a los metodos
+        En el constructor instancia la fecha actual y ese dato establece el estado interno del objeto
 
-        //POR QUE CONSIDERO QUE ES UNA CLASE NO ANEMICA:
-        //-----> Encapsula un estado interno y que se inicializa en el constructor
-        //-----> Los metodos implementados utilizan el estado interno en vez de instanciar variables en cada llamada a los metodos
+        POR QUE CONSIDERO QUE ES UNA CLASE NO ANEMICA:
+        -----> Encapsula un estado interno y que se inicializa en el constructor
+        -----> Los metodos implementados utilizan el estado interno, es decir, aplican la lógica relacionada al objeto toda
+                dentro del mismo objeto.
+        -----> Es el mismo objeto el que conoce y maneja su propia lógica sobre su estado interno
 
+         */
 
         System.out.println("Fecha de hoy en formato corto: " + a.fechaFormatoCorto());
         System.out.println("Fecha de hoy en formato largo: " + a.fechaFormatoLargo());
